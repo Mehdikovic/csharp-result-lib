1ef888c35d73a97474165813e20563b0258269a4
Mehmovic <che.mehdi.zarei@gmail.com>
2025-04-20 22:11:11 +0330

    [Add] unit test csproj

---
+++ ResultLib.Test/src/Result_UnitTest.cs
@@ -0,0 +1,18 @@
+﻿namespace ResultLib.Test;
+
+public class Tests {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test_DefaultConstructorShouldBeInStateError() {
+        var result = new Result();
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.IsError());
+            Assert.That(!result.IsOk());
+        });
+    }
+}
